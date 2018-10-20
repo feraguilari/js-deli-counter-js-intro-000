a938db49f806ff71abbdf40b5b0d6871aa86134e
Fernando Aguilar Islas <f.aguislas@gmail.com>
2018-10-20 23:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,30 +18,8 @@ function currentLine(line){
   }
   else {
     for (var i = 0; i < line.length; i++) {
-      customers.push += (i + 1) + ". " + line[i];
+      customers.push((i + 1) + ". " + line[i])
     }
     return "The line is currently: " + customers
   }
-}
-/*function currentLine(x) {
-  var line = []
-    if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        var line += (i + 1) + ". " + x[i] + ", "
-      }
-      return "The line is currently: " + line
-    }
-}*/
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
 }
